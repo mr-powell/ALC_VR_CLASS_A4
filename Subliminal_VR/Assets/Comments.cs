213278efc3d8f4263c80414e25b1750354a64bda
JFordhamALC <jax.f814@stu.nebo.edu>
2022-10-27 12:59:09 -0600

    IDK

---
+++ Subliminal_VR/Assets/Comments.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Movement:Done
+//Hands:Done
+//Interaction with Objects
+//Flashlight
+//Key:Lock
+
+
+public class Comments : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
